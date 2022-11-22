f8d3cd8ca6bc8c2cca132e09cf94d29851b32898
Slade Watkins <srw@sladewatkins.net>
2022-11-22 12:46:04 -0500

    templates: page: add page template
    Add the page template, which is a skeleton for pages on the website.
    
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

---
+++ files/templates/page.php
@@ -0,0 +1,19 @@
+<html lang="en-us">
+    <head>
+        <title>Slade Rice Watkins</title>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/files/head.php";
+   include_once($path);
+?>
+    </head>
+
+    <body>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/files/nav.php";
+   include_once($path);
+?>
+        <p>This is a template page. Replace this with the content you want here instead.</p>
+    </body>
+</html>
