6db67a8c107a61539adaab49f8f51b012a3ed77c
Slade Watkins <srw@sladewatkins.net>
2022-11-22 10:14:40 -0500

    index: add index file
    index.php does not exist, add and push to remote repo on Github.
    
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="en-us">
+    <head>
+        <title>Slade Rice Watkins</title>
+        <link rel="stylesheet" href="/files/style.css">
+    </head>
+
+    <body>
+        <p>This is the homepage.</p>
+    </body>
+</html>
