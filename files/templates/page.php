e751738aa697b6dd97250fbd407b5d3a2a906e2a
Slade Watkins <srw@sladewatkins.net>
2022-11-24 01:14:54 -0500

    templates: page: add seo to template
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- files/templates/page.php
+++ files/templates/page.php
@@ -6,6 +6,17 @@
    $path .= "/files/head.php";
    include_once($path);
 ?>
+<?php 
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/files/seo.php";
+   include_once($path);
+?>
+    <link rel="canonical" href="https://www.sladewatkins.com/" />
+    <meta property="twitter:title" content="Slade Rice Watkins" />
+    <meta property="og:title" content="Slade Rice Watkins" />
+    <meta property="description" content="The website of Slade Rice Watkins: Editor-in-Chief, Director, Founder of Kakariko Herald. Web developer." />
+    <meta property="twitter:description" content="The website of Slade Rice Watkins: Editor-in-Chief, Director, Founder of Kakariko Herald. Web developer." />
+    <meta property="og:description" content="The website of Slade Rice Watkins: Editor-in-Chief, Director, Founder of Kakariko Herald. Web developer." />
     </head>
 
     <body>
