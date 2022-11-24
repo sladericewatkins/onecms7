496575ea938b4919bdbd3e67d6952a86f3a83755
Slade Watkins <srw@sladewatkins.net>
2022-11-24 01:15:55 -0500

    seo: update references to Blo(n)g(er) and old name
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- files/seo.php
+++ files/seo.php
@@ -1,5 +1,5 @@
-    <meta name="generator" content="Slade Watkins" />
-    <meta name="author" content="Slade Watkins" />
+    <meta name="generator" content="Slade Rice Watkins" />
+    <meta name="author" content="Slade Rice Watkins" />
     <meta property="og:locale" content="en_US" />
-    <meta property="og:site_name" content="Slade’s Blo(n)g(er)" />
+    <meta property="og:site_name" content="Slade Rice Watkins" />
     <meta property="og:type" content="website" />
