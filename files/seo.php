b8b94c9559e14a2e49579446d161b3894b85ea5c
Slade Watkins <srw@sladewatkins.net>
2022-11-24 01:11:03 -0500

    add seo dependency file
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

---
+++ files/seo.php
@@ -0,0 +1,7 @@
+    <meta name="generator" content="Slade Watkins" />
+    <meta name="author" content="Slade Watkins" />
+    <meta property="og:locale" content="en_US" />
+    <link rel="canonical" href="https://www.sladewatkins.com/" />
+    <meta property="og:url" content="https://www.sladewatkins.com/" />
+    <meta property="og:site_name" content="Slade’s Blo(n)g(er)" />
+    <meta property="og:type" content="website" />
