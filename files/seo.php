01e03ece8ca55564a6d5228555d648eb0d05a498
Slade Watkins <srw@sladewatkins.net>
2022-12-03 06:52:16 -0500

    Remove all legacy dev files
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- files/seo.php
+++
@@ -1,5 +0,0 @@
-    <meta name="generator" content="Slade Rice Watkins" />
-    <meta name="author" content="Slade Rice Watkins" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:site_name" content="Slade Rice Watkins" />
-    <meta property="og:type" content="website" />
