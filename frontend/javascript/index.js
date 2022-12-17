63a2b4ba6923c1b828c3a984e06188d9093840f1
Slade Watkins <srw@sladewatkins.net>
2022-12-17 15:10:55 -0500

    meta: OneCMS 7.0.10
    Tested-by: Post Bot <postbot@sladewatkins.net>
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,7 +13,7 @@ import { Application } from "@hotwired/stimulus"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
+console.info("OneCMS 7.0.10 loaded!")
 
 window.Stimulus = Application.start()
 
