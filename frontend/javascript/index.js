87edb22089570a2da6401a21afe7a49521c8fc0c
Slade Watkins <srw@sladewatkins.net>
2023-01-01 12:58:02 -0500

    meta: OneCMS 7.0.19
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,7 +13,7 @@ import { Application } from "@hotwired/stimulus"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("OneCMS 7.0.18 loaded!")
+console.info("OneCMS 7.0.19 loaded!")
 
 window.Stimulus = Application.start()
 
