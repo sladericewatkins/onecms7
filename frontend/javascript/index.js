d34830ac1729e14fb0c7862f17dff74f44de277f
Slade Watkins <srw@sladewatkins.net>
2023-01-29 17:06:50 -0500

    OneCMS 7.2.0
    This is the first initial release of OneCMS 7.2.0.
    
    What's new:
    
    RC1:
    - updates ruby version to 3.2.0
    - removes references to pope.party
    - updates the base to upstream's Bridgetown 1.2.0 release
    - documentation updates
    - accessibility updates
    
    RC2:
    - navbar fix
    - dependency updates
    - documentation updates
    
    RELEASE:
    - shrink size of turbo progress bar slightly
    - add "byline: stylization for use with blonger/writing layouts
    
    Closes: #38
    Tested-by: Slade Watkins <srw@sladewatkins.net>
    Tested-by: Post Bot <postbot@sladewatkins.net>
    Reviewed-by: Post Bot <postbot@sladewatkins.net>
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,7 +13,7 @@ import { Application } from "@hotwired/stimulus"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("OneCMS 7.2.0-rc2 loaded!")
+console.info("OneCMS 7.2.0 loaded!")
 
 window.Stimulus = Application.start()
 
