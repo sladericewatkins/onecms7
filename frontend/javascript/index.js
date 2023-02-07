f9a2cd3b5d6d584cc7e9680083ad215c1f7b34d1
Slade Watkins <srw@sladewatkins.net>
2023-02-07 09:37:18 -0500

    OneCMS 7.1.26
    What's new:
    - dependency updates (from #47)
    
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,7 +13,7 @@ import { Application } from "@hotwired/stimulus"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("OneCMS 7.1.25 loaded!")
+console.info("OneCMS 7.1.26 loaded!")
 
 window.Stimulus = Application.start()
 
