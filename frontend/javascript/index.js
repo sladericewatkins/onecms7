2559f5a16ba2afeb796dde6631bce65de8ec44ff
Slade Watkins <srw@sladewatkins.net>
2023-04-07 20:05:21 -0400

    OneCMS 7.2.16
    Signed-off-by: Slade Watkins <srw@sladewatkins.net>

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -13,7 +13,7 @@ import { Application } from "@hotwired/stimulus"
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("OneCMS 7.2.15 loaded!")
+console.info("OneCMS 7.2.16 loaded!")
 
 window.Stimulus = Application.start()
 
